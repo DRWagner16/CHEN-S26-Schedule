8a9524d39fa73b5f3f1470507320b406b7290f6e
DRWagner16 <drwagner@chemeng.utah.edu>
2025-06-16 14:34:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function fetchDataAndInitialize() {
-        fetch('S25schedule.json')
+        fetch('S26schedule.json')
             .then(response => response.json())
             .then(data => {
                 allCourses = data.map(course => {
